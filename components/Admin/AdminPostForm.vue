ade8e77fb6cfd8932a29935d05da5d6ca52725dc
Mikhail Zinchuk <viper92@inbox.ru>
2023-03-09 21:35:37 +0100

    feat: add firebase

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -6,12 +6,16 @@
         
         <AppControlInput v-model="editedPost.title">Title</AppControlInput>
         
-        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
         
         <AppControlInput
           control-type="textarea"
           v-model="editedPost.content">Content</AppControlInput>
         
+        <AppControlInput
+          control-type="textarea"
+          v-model="editedPost.previewText">Preview text</AppControlInput>
+
         <AppButton type="submit">Save</AppButton>
         
         <AppButton
@@ -41,15 +45,16 @@ export default {
       editedPost: this.post ? { ...this.post} : {
         author: '',
         title: '',
-        thumbnailLink: '',
-        content: ''
+        thumbnail: '',
+        content: '',
+        previewText: ''
 
       }
     }
   },
   methods: {
     onSave() {
-      console.log(this.editedPost)
+      this.$emit('submit', this.editedPost)
     },
     onCancel() {
       this.$router.push('/admin')
--- store/index.js
+++ store/index.js
@@ -1,49 +1,38 @@
-import Vuex from 'vuex'
+import Vuex from "vuex";
+import axios from "axios";
 
-
-const createStore = ()=>{
+const createStore = () => {
   return new Vuex.Store({
     state: {
       loadedPosts: []
     },
     mutations: {
-      setPosts(state,posts) {
-        state.loadedPosts = posts
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
       }
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve,reject)=>{
-          setTimeout(()=>{
-            vuexContext.commit('setPosts',  [
-              {
-                id:'1', 
-                title: 'First post', 
-                previewText: 'This is my first post', 
-                thumbnail: 'https://www.fidelity.com/bin-public/060_www_fidelity_com/images/tech-stocks-twitter.png'
-            },
-              {
-                id:'2', 
-                title: 'Second post', 
-                previewText: 'This is my second post', thumbnail: 'https://www.fidelity.com/bin-public/060_www_fidelity_com/images/tech-stocks-twitter.png'},
-              {
-              id:'3', 
-              title: 'Third post', 
-              previewText: 'This is my third post', 
-              thumbnail: 'https://www.fidelity.com/bin-public/060_www_fidelity_com/images/tech-stocks-twitter.png'},])
-            resolve() 
-              },1000)
-      })},
+        return axios.get('https://nuxt-blog-e1acb-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+          .then(res => {
+            const postsArray = []
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
+            vuexContext.commit('setPosts', postsArray)
+          })
+          .catch(e => context.error(e));
+      },
       setPosts(vuexContext, posts) {
-        vuexContext.commit('setPosts', posts)
+        vuexContext.commit("setPosts", posts);
       }
     },
     getters: {
       loadedPosts(state) {
-        return state.loadedPosts
+        return state.loadedPosts;
       }
     }
-  })
-}
+  });
+};
 
-export default createStore
+export default createStore;
